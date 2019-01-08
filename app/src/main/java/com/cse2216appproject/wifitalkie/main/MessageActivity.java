bf795f9b5c31a818817f74ae7e236a4141772b8d
farhantanvirtushar <farhantanvirtushar@users.noreply.github.com>
2019-01-09 00:15:29 +0600

    recyclerview updated

--- app/src/main/java/com/cse2216appproject/wifitalkie/main/MessageActivity.java
+++ app/src/main/java/com/cse2216appproject/wifitalkie/main/MessageActivity.java
@@ -5,23 +5,31 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.cse2216appproject.wifitalkie.R;
 import com.cse2216appproject.wifitalkie.support.Data;
+import com.cse2216appproject.wifitalkie.support.MyRecyclerViewAdapter;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class MessageActivity extends AppCompatActivity {
 
     private SendReceive sendReceive;
     private EditText writeMessage;
-    private TextView readMessageBox;
+    //private TextView readMessageBox;
+
+    RecyclerView recyclerView;
+    MyRecyclerViewAdapter adapter;
+    ArrayList<String> sentMessages;
 
     private static final int MESSAGE_READ = 1;
 
@@ -30,14 +38,23 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-        readMessageBox = findViewById(R.id.readMessage);
+        //readMessageBox = findViewById(R.id.readMessage);
         writeMessage = findViewById(R.id.writeMessage);
         sendReceive = new SendReceive(Data.socket);
+
+        sentMessages = new ArrayList<>();
+        // set up the RecyclerView
+        recyclerView = findViewById(R.id.chat);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, sentMessages);
+        recyclerView.setAdapter(adapter);
     }
 
     public void sendMessage(View view) {
         String dataToSend = writeMessage.getText().toString();
         sendReceive.writeTo(dataToSend);
+        sentMessages.add(dataToSend);
+        recyclerView.refreshDrawableState();
     }
 
     Handler handler = new Handler(new Handler.Callback() {
@@ -47,7 +64,10 @@ public class MessageActivity extends AppCompatActivity {
                 case MESSAGE_READ:
                     byte[] readBuff = (byte[]) message.obj;
                     String tempMessage = new String(readBuff, 0, message.arg1);
-                    readMessageBox.setText(tempMessage);
+                    //readMessageBox.setText(tempMessage);
+                    sentMessages.add(tempMessage);
+                    recyclerView.refreshDrawableState();
+
                     break;
             }
             return true;
